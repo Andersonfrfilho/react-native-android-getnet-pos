f5ec5819e38103e7da81e4d7afe1c72fa243c397
Anderson <andersonfrfilho@gmail.com>
2024-05-28 15:22:12 -0300

    feat: add apk and instructions to run in emulator

--- README.md
+++ README.md
@@ -29,3 +29,14 @@ MIT
 ---
 
 Made with [create-react-native-library](https://github.com/callstack/react-native-builder-bob)
+
+Step to use
+- install serviceposdigital-2.2.2-30-realses.apk
+  - Option 1 Genymotion
+    - Install Genymotion
+    - Create Android device with 5.1 (Pixel 2)
+    - Install Genymotion ARM Translator
+      - download https://github.com/m9rco/Genymotion_ARM_Translation/blob/master/package/Genymotion-ARM-Translation_for_5.1.zip
+      - drag and drop in emulator or Acess-Link(https://stackoverflow.com/questions/58459833/unable-to-install-apk-in-android-emulator-app-contains-arm-native-code)
+  - Option 2 Emulator native
+    -
--- android/src/main/java/com/getnetpos/GetnetPosModule.kt
+++ android/src/main/java/com/getnetpos/GetnetPosModule.kt
@@ -5,12 +5,38 @@ import com.facebook.react.bridge.ReactContextBaseJavaModule
 import com.facebook.react.bridge.ReactMethod
 import com.facebook.react.bridge.Promise
 import com.getnet.posdigital.PosDigital
+import android.util.Log
 
 class GetnetPosModule(reactContext: ReactApplicationContext) :
   ReactContextBaseJavaModule(reactContext) {
+  companion object {
+    public const val TAG = "GetnetPosModuleReactNative"
+    public const val NAME = "GetnetPos"
+  }
 
   override fun getName(): String {
     return NAME
+  }
+
+  private val bindCallback: PosDigital.BindCallback
+    get() = object : PosDigital.BindCallback {
+      override fun onError(e: Exception) {
+        if (PosDigital.getInstance().isInitiated)
+          PosDigital.unregister(getReactApplicationContext())
+        connectPosDigitalService()
+      }
+      override fun onConnected() {
+
+        Log.d(TAG, "connected uhull zo/")
+      }
+
+      override fun onDisconnected() {
+        Log.d(TAG, "disconnected :X")
+      }
+    }
+
+  private fun connectPosDigitalService() {
+    PosDigital.register(getReactApplicationContext(), bindCallback)
   }
 
   // Example method
@@ -19,9 +45,5 @@ class GetnetPosModule(reactContext: ReactApplicationContext) :
   fun multiply(a: Double, b: Double, promise: Promise) {
 
     promise.resolve(a * b)
-  }
-
-  companion object {
-    const val NAME = "GetnetPos"
   }
 }
